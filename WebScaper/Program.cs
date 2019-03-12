32a46b867718750daf2b5fa0e47a2c81998a72d6
blackmage37 <ben.watsaEapW6roXmllon@nfu.org.uk>
2019-03-12 14:06:26 +0000

    attempted to handle team total calculation when there are poorly formatted lines, too much trouble. aborted attempt and removed this functionality for now

--- WebScaper/Program.cs
+++ WebScaper/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Configuration;
 using HtmlAgilityPack;
+using System.Text.RegularExpressions;
 
 namespace WebScaper
 {
@@ -26,25 +27,25 @@ namespace WebScaper
 
                 Scrape(web, ConfigurationManager.AppSettings["FA1"].ToString(), ConfigurationManager.AppSettings["FA2"].ToString(), ConfigurationManager.AppSettings["FA3"].ToString(), "FA", "Free Agents");
 
-            //Scrape(web, ConfigurationManager.AppSettings["KC1"].ToString(), ConfigurationManager.AppSettings["KC2"].ToString(), ConfigurationManager.AppSettings["KC3"].ToString(), "KC", "Kansas City Coyotes");
-            //Scrape(web, ConfigurationManager.AppSettings["PO1"].ToString(), ConfigurationManager.AppSettings["PO2"].ToString(), ConfigurationManager.AppSettings["PO3"].ToString(), "PO", "Portland Pythons");
-            //Scrape(web, ConfigurationManager.AppSettings["SA1"].ToString(), ConfigurationManager.AppSettings["SA2"].ToString(), ConfigurationManager.AppSettings["SA3"].ToString(), "SA", "San Antonio Marshals");
-            //Scrape(web, ConfigurationManager.AppSettings["TL1"].ToString(), ConfigurationManager.AppSettings["TL2"].ToString(), ConfigurationManager.AppSettings["TL3"].ToString(), "TL", "Tijuana Luchadores");
-            //Scrape(web, ConfigurationManager.AppSettings["PB1"].ToString(), ConfigurationManager.AppSettings["PB2"].ToString(), ConfigurationManager.AppSettings["PB3"].ToString(), "PB", "Palm Beach Solar Bears");
-            //Scrape(web, ConfigurationManager.AppSettings["NS1"].ToString(), ConfigurationManager.AppSettings["NS2"].ToString(), ConfigurationManager.AppSettings["NS3"].ToString(), "NS", "Norfolk Seawolves");
+                Scrape(web, ConfigurationManager.AppSettings["KC1"].ToString(), ConfigurationManager.AppSettings["KC2"].ToString(), ConfigurationManager.AppSettings["KC3"].ToString(), "KC", "Kansas City Coyotes");
+                Scrape(web, ConfigurationManager.AppSettings["PO1"].ToString(), ConfigurationManager.AppSettings["PO2"].ToString(), ConfigurationManager.AppSettings["PO3"].ToString(), "PO", "Portland Pythons");
+                Scrape(web, ConfigurationManager.AppSettings["SA1"].ToString(), ConfigurationManager.AppSettings["SA2"].ToString(), ConfigurationManager.AppSettings["SA3"].ToString(), "SA", "San Antonio Marshals");
+                Scrape(web, ConfigurationManager.AppSettings["TL1"].ToString(), ConfigurationManager.AppSettings["TL2"].ToString(), ConfigurationManager.AppSettings["TL3"].ToString(), "TL", "Tijuana Luchadores");
+                Scrape(web, ConfigurationManager.AppSettings["PB1"].ToString(), ConfigurationManager.AppSettings["PB2"].ToString(), ConfigurationManager.AppSettings["PB3"].ToString(), "PB", "Palm Beach Solar Bears");
+                Scrape(web, ConfigurationManager.AppSettings["NS1"].ToString(), ConfigurationManager.AppSettings["NS2"].ToString(), ConfigurationManager.AppSettings["NS3"].ToString(), "NS", "Norfolk Seawolves");
 
-            //Scrape(web, ConfigurationManager.AppSettings["QB1"].ToString(), ConfigurationManager.AppSettings["KC2"].ToString(), ConfigurationManager.AppSettings["KC3"].ToString(), "QB", "Quarterbacks");
-            //Scrape(web, ConfigurationManager.AppSettings["RB1"].ToString(), ConfigurationManager.AppSettings["KC2"].ToString(), ConfigurationManager.AppSettings["KC3"].ToString(), "RB", "Runningbacks");
-            //Scrape(web, ConfigurationManager.AppSettings["WR1"].ToString(), ConfigurationManager.AppSettings["KC2"].ToString(), ConfigurationManager.AppSettings["KC3"].ToString(), "WR", "Wide Receivers");
-            //Scrape(web, ConfigurationManager.AppSettings["TE1"].ToString(), ConfigurationManager.AppSettings["KC2"].ToString(), ConfigurationManager.AppSettings["KC3"].ToString(), "TE", "Tight Ends");
-            //Scrape(web, ConfigurationManager.AppSettings["OL1"].ToString(), ConfigurationManager.AppSettings["KC2"].ToString(), ConfigurationManager.AppSettings["KC3"].ToString(), "OL", "Offensive Line");
-            //Scrape(web, ConfigurationManager.AppSettings["DE1"].ToString(), ConfigurationManager.AppSettings["KC2"].ToString(), ConfigurationManager.AppSettings["KC3"].ToString(), "DE", "Defensive Ends");
-            //Scrape(web, ConfigurationManager.AppSettings["DT1"].ToString(), ConfigurationManager.AppSettings["KC2"].ToString(), ConfigurationManager.AppSettings["KC3"].ToString(), "DT", "Defensive Tackles");
-            //Scrape(web, ConfigurationManager.AppSettings["LB1"].ToString(), ConfigurationManager.AppSettings["KC2"].ToString(), ConfigurationManager.AppSettings["KC3"].ToString(), "LB", "Linebackers");
-            //Scrape(web, ConfigurationManager.AppSettings["CB1"].ToString(), ConfigurationManager.AppSettings["KC2"].ToString(), ConfigurationManager.AppSettings["KC3"].ToString(), "CB", "Cornerbacks");
-            //Scrape(web, ConfigurationManager.AppSettings["SF1"].ToString(), ConfigurationManager.AppSettings["KC2"].ToString(), ConfigurationManager.AppSettings["KC3"].ToString(), "SF", "Safeties");
-            //Scrape(web, ConfigurationManager.AppSettings["KP1"].ToString(), ConfigurationManager.AppSettings["KC2"].ToString(), ConfigurationManager.AppSettings["KC3"].ToString(), "KP", "Kickers and Punters");
-            
+                Scrape(web, ConfigurationManager.AppSettings["QB1"].ToString(), ConfigurationManager.AppSettings["KC2"].ToString(), ConfigurationManager.AppSettings["KC3"].ToString(), "QB", "Quarterbacks");
+                Scrape(web, ConfigurationManager.AppSettings["RB1"].ToString(), ConfigurationManager.AppSettings["KC2"].ToString(), ConfigurationManager.AppSettings["KC3"].ToString(), "RB", "Runningbacks");
+                Scrape(web, ConfigurationManager.AppSettings["WR1"].ToString(), ConfigurationManager.AppSettings["KC2"].ToString(), ConfigurationManager.AppSettings["KC3"].ToString(), "WR", "Wide Receivers");
+                Scrape(web, ConfigurationManager.AppSettings["TE1"].ToString(), ConfigurationManager.AppSettings["KC2"].ToString(), ConfigurationManager.AppSettings["KC3"].ToString(), "TE", "Tight Ends");
+                Scrape(web, ConfigurationManager.AppSettings["OL1"].ToString(), ConfigurationManager.AppSettings["KC2"].ToString(), ConfigurationManager.AppSettings["KC3"].ToString(), "OL", "Offensive Line");
+                Scrape(web, ConfigurationManager.AppSettings["DE1"].ToString(), ConfigurationManager.AppSettings["KC2"].ToString(), ConfigurationManager.AppSettings["KC3"].ToString(), "DE", "Defensive Ends");
+                Scrape(web, ConfigurationManager.AppSettings["DT1"].ToString(), ConfigurationManager.AppSettings["KC2"].ToString(), ConfigurationManager.AppSettings["KC3"].ToString(), "DT", "Defensive Tackles");
+                Scrape(web, ConfigurationManager.AppSettings["LB1"].ToString(), ConfigurationManager.AppSettings["KC2"].ToString(), ConfigurationManager.AppSettings["KC3"].ToString(), "LB", "Linebackers");
+                Scrape(web, ConfigurationManager.AppSettings["CB1"].ToString(), ConfigurationManager.AppSettings["KC2"].ToString(), ConfigurationManager.AppSettings["KC3"].ToString(), "CB", "Cornerbacks");
+                Scrape(web, ConfigurationManager.AppSettings["SF1"].ToString(), ConfigurationManager.AppSettings["KC2"].ToString(), ConfigurationManager.AppSettings["KC3"].ToString(), "SF", "Safeties");
+                Scrape(web, ConfigurationManager.AppSettings["KP1"].ToString(), ConfigurationManager.AppSettings["KC2"].ToString(), ConfigurationManager.AppSettings["KC3"].ToString(), "KP", "Kickers and Punters");
+
             // store a timestamp in record.json, then clean up
             var dt = DateTime.UtcNow;
                 System.IO.StreamWriter dtFile = new System.IO.StreamWriter(ConfigurationManager.AppSettings["LocalPath"] + "record.json");
@@ -125,19 +126,47 @@ namespace WebScaper
 
             CloseAndDispose(file1);
 
+/* commenting out the "team total" section for now. might be best to calculate this on publish instead
+ * 
             string[] SJSTPETotal = System.IO.File.ReadAllLines(ConfigurationManager.AppSettings["LocalPath"] + teamAbrv+"Players.txt");
             List<int> Total = new List<int>();
             foreach (string line in SJSTPETotal)
             {
-                string[] splitwords = line.Split(',');
-                string[] splitAgain = splitwords[1].Split(':');
-                Total.Add(Convert.ToInt32(splitAgain[1]));
+                // only process lines that start with the player season number in format S#, brackets optional at this point
+                Regex regex = new Regex(@".*[sS]\d+");
+                Match regexMatch = regex.Match(line.Substring(0, 5));
+                if(regexMatch.Success)
+                {
+                    
+                    string[] splitwords = line.Split(',');
+                    string[] splitAgain;
+
+                    // check each part of the split for the "TPE: 000" statement
+                    // this should stop the scraper crashing when titles are not in the correct format
+                    foreach (var segment in splitwords)
+                    {
+                        if (segment.IndexOf("TPE") > 0)
+                        {
+                            splitAgain = segment.Split(' ');
+                            Total.Add(Convert.ToInt32(splitAgain[splitAgain.Length-1]));
+                        }
+                    
+                    }
+
+                    //string[] splitAgain = splitwords[1].Split(':');
+                    //Total.Add(Convert.ToInt32(splitAgain[1]));
+
+                }
+
             }
 
             System.IO.StreamWriter file2 = new System.IO.StreamWriter(ConfigurationManager.AppSettings["LocalPath"] + teamAbrv+"Team.txt");
 
             file2.WriteLine(teamName+"' TPE: " + Total.Sum());
             CloseAndDispose(file2);
+
+ */
+
         }
 
         private static void CloseAndDispose(System.IO.StreamWriter file1)
