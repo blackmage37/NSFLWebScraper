de1662606c11877878ab80a405bb737feb3c61b0
blackmage37 <ben.watsaEapW6roXmllon@nfu.org.uk>
2019-03-26 12:22:40 +0000

    added process to populate AllPlayers.txt, scraper should now ignore any threads not starting (S#)

--- WebScraper/Program.cs
+++ WebScraper/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Configuration;
 using HtmlAgilityPack;
 using System.Text.RegularExpressions;
@@ -14,8 +13,14 @@ namespace WebScraper
 
         static void Main(string[] args)
         {
-            HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
+            HtmlWeb web = new HtmlWeb();
 
+            // clear the AllPlayers file ahead of the append operations when scraping
+            File.WriteAllText(ConfigurationManager.AppSettings["LocalPath"] + "AllPlayers.txt", string.Empty);
+
+            // start scraping...
+
+                // NSFL
                 Scrape(web, ConfigurationManager.AppSettings["SJS1"].ToString(), ConfigurationManager.AppSettings["SJS2"].ToString(), ConfigurationManager.AppSettings["SJS3"].ToString(), "SJS", "San Jose Sabercats");
                 Scrape(web, ConfigurationManager.AppSettings["BH1"].ToString(), ConfigurationManager.AppSettings["BH2"].ToString(), ConfigurationManager.AppSettings["BH3"].ToString(), "BH", "Baltimore Hawks");
                 Scrape(web, ConfigurationManager.AppSettings["CY1"].ToString(), ConfigurationManager.AppSettings["CY2"].ToString(), ConfigurationManager.AppSettings["CY3"].ToString(), "CY", "Colorado Yetis");
@@ -25,15 +30,18 @@ namespace WebScraper
                 Scrape(web, ConfigurationManager.AppSettings["NO1"].ToString(), ConfigurationManager.AppSettings["NO2"].ToString(), ConfigurationManager.AppSettings["NO3"].ToString(), "NO", "New Orleans Second Line");
                 Scrape(web, ConfigurationManager.AppSettings["OCO1"].ToString(), ConfigurationManager.AppSettings["OCO2"].ToString(), ConfigurationManager.AppSettings["OCO3"].ToString(), "OCO", "Orange County Otters");
 
+                // Free Agents
                 Scrape(web, ConfigurationManager.AppSettings["FA1"].ToString(), ConfigurationManager.AppSettings["FA2"].ToString(), ConfigurationManager.AppSettings["FA3"].ToString(), "FA", "Free Agents");
-
+                
+                // DSFL
                 Scrape(web, ConfigurationManager.AppSettings["KC1"].ToString(), ConfigurationManager.AppSettings["KC2"].ToString(), ConfigurationManager.AppSettings["KC3"].ToString(), "KC", "Kansas City Coyotes");
                 Scrape(web, ConfigurationManager.AppSettings["PO1"].ToString(), ConfigurationManager.AppSettings["PO2"].ToString(), ConfigurationManager.AppSettings["PO3"].ToString(), "PO", "Portland Pythons");
                 Scrape(web, ConfigurationManager.AppSettings["SA1"].ToString(), ConfigurationManager.AppSettings["SA2"].ToString(), ConfigurationManager.AppSettings["SA3"].ToString(), "SA", "San Antonio Marshals");
                 Scrape(web, ConfigurationManager.AppSettings["TL1"].ToString(), ConfigurationManager.AppSettings["TL2"].ToString(), ConfigurationManager.AppSettings["TL3"].ToString(), "TL", "Tijuana Luchadores");
                 Scrape(web, ConfigurationManager.AppSettings["PB1"].ToString(), ConfigurationManager.AppSettings["PB2"].ToString(), ConfigurationManager.AppSettings["PB3"].ToString(), "PB", "Palm Beach Solar Bears");
                 Scrape(web, ConfigurationManager.AppSettings["NS1"].ToString(), ConfigurationManager.AppSettings["NS2"].ToString(), ConfigurationManager.AppSettings["NS3"].ToString(), "NS", "Norfolk Seawolves");
-
+            
+                // Prospects
                 Scrape(web, ConfigurationManager.AppSettings["QB1"].ToString(), ConfigurationManager.AppSettings["QB2"].ToString(), ConfigurationManager.AppSettings["QB3"].ToString(), "QB", "Quarterbacks");
                 Scrape(web, ConfigurationManager.AppSettings["RB1"].ToString(), ConfigurationManager.AppSettings["RB2"].ToString(), ConfigurationManager.AppSettings["RB3"].ToString(), "RB", "Runningbacks");
                 Scrape(web, ConfigurationManager.AppSettings["WR1"].ToString(), ConfigurationManager.AppSettings["WR2"].ToString(), ConfigurationManager.AppSettings["WR3"].ToString(), "WR", "Wide Receivers");
@@ -48,17 +56,17 @@ namespace WebScraper
 
             // store a timestamp in record.json, then clean up
             var dt = DateTime.UtcNow;
-                System.IO.StreamWriter dtFile = new System.IO.StreamWriter(ConfigurationManager.AppSettings["LocalPath"] + "record.json");
-                dtFile.WriteLine(dt.ToString("ddd, d MMMM yyyy HH:mm:ss zzz"));
+                StreamWriter dtFile = new StreamWriter(ConfigurationManager.AppSettings["LocalPath"] + "record.json");
+                dtFile.WriteLine(dt.ToString("ddd, d MMMM yyyy HH:mm:ss o"));
                 CloseAndDispose(dtFile);
 
         }
 
         private static void Scrape(HtmlWeb web,string d1, string d2, string d3, string teamAbrv, string teamName)
         {
-            HtmlAgilityPack.HtmlDocument doc = web.Load(d1);
-            HtmlAgilityPack.HtmlDocument doc2 = web.Load(d2);
-            HtmlAgilityPack.HtmlDocument doc3 = null;
+            HtmlDocument doc = web.Load(d1);
+            HtmlDocument doc2 = web.Load(d2);
+            HtmlDocument doc3 = null;
             int pagecount;
             HtmlNodeCollection pagination = doc.DocumentNode.SelectNodes(ConfigurationManager.AppSettings["PaginationPage"]);
             if (pagination == null)
@@ -119,7 +127,10 @@ namespace WebScraper
 
             string subfolder = GetSubfolder(teamAbrv);
 
-            System.IO.StreamWriter file1 = new System.IO.StreamWriter(ConfigurationManager.AppSettings["LocalPath"] + subfolder + teamAbrv+"Players.txt");
+            // create a path to the players file and open the existing AllPlayers file in append mode
+            StreamWriter file1 = new StreamWriter(ConfigurationManager.AppSettings["LocalPath"] + subfolder + teamAbrv + "Players.txt");
+            StreamWriter file3 = File.AppendText(ConfigurationManager.AppSettings["LocalPath"] + "AllPlayers.txt");
+
             foreach (var nt in NameAndTPEAndURL)
             {
 
@@ -127,12 +138,20 @@ namespace WebScraper
                 string sName = nt.NameAndTPE.PlayerNames.InnerText;
                 sName.Replace("–", "-");
 
-                // construct player line and output to text file
-                file1.WriteLine(sName + "," + nt.NameAndTPE.PlayerTPE.InnerText + ", " + nt.href1.ToString() + ",");
+                // only process lines that start with the player season number in format (S#)
+                Regex regex = new Regex(@"\([sS]\d+\)");
+                Match regexMatch = regex.Match(sName.Substring(0, 5));
+                if (regexMatch.Success)
+                {
+                    // construct player line and output to text file (and "allplayers" file)
+                    file1.WriteLine(sName + "," + nt.NameAndTPE.PlayerTPE.InnerText + ", " + nt.href1.ToString() + ",");
+                    file3.WriteLine(sName + "," + nt.NameAndTPE.PlayerTPE.InnerText + ", " + nt.href1.ToString() + ",");
+                }
 
             }
-
+            
             CloseAndDispose(file1);
+            CloseAndDispose(file3);
 
 /* commenting out the "team total" section for now. might be best to calculate this on publish instead
  * 
@@ -220,10 +239,10 @@ namespace WebScraper
             }
         }
 
-        private static void CloseAndDispose(System.IO.StreamWriter file1)
+        private static void CloseAndDispose(StreamWriter fileX)
         {
-            file1.Close();
-            file1.Dispose();
+            fileX.Close();
+            fileX.Dispose();
         }
 
         private static List<HtmlNode> GetNodes(HtmlDocument document, string config)
@@ -237,7 +256,7 @@ namespace WebScraper
             int switcher = 1;
             foreach (HtmlNode node in document.DocumentNode.SelectNodes(ConfigurationManager.AppSettings["PlayerURL"]))
             {
-                String[] x = node.OuterHtml.Split('"');
+                string[] x = node.OuterHtml.Split('"');
 
                     if (switcher == 1)
                     {
@@ -252,7 +271,7 @@ namespace WebScraper
             }
         }
 
-        private static void RemoveRepliesFromPlayerNames(List<HtmlAgilityPack.HtmlNode> PlayerNames)
+        private static void RemoveRepliesFromPlayerNames(List<HtmlNode> PlayerNames)
         {
             int pos = 0;
             for (int i = 0; i < PlayerNames.Count; i += 2, pos++)
